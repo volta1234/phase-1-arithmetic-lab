dfd0f1060ab2dadf644bae4efa9330c77eef7897
CALVINCE <calvince.ogwel@gmail.com>
2022-11-23 19:15:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
-// Write your code here
+let random = Number.isInteger(Math.random()* 10)+1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3%num4;
+
+let max = Math.max(12,6,7,4,15.17,19,20)
+max;
